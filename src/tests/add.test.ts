b89e5f215417880077520690a58051c0929ea277
aguyran <aguyran@gmail.com>
2024-07-29 00:38:09 +0530

    add basic sum of numbers separated by comma

--- src/tests/add.test.ts
+++ src/tests/add.test.ts
@@ -3,3 +3,8 @@ import add from "./add";
 test("parses empty string as 0", () => {
   expect(add("")).toBe(0);
 });
+
+test("parses filled string with commas as their sum", () => {
+  expect(add("1")).toBe(1);
+  expect(add("1,5")).toBe(6);
+});
--- src/tests/add.ts
+++ src/tests/add.ts
@@ -2,7 +2,10 @@ function add(numbers: string): number {
   if (!numbers) {
     return 0;
   }
-  return 0;
+  return numbers
+    .split(",")
+    .map(Number)
+    .reduce((acc, curr) => acc + curr, 0);
 }
 
 export default add;
