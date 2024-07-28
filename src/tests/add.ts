1bb3d80fb7ed6893c56aaf143610b93825c025b8
aguyran <aguyran@gmail.com>
2024-07-29 01:12:23 +0530

    refactor delimeter code

--- src/tests/add.ts
+++ src/tests/add.ts
@@ -2,19 +2,25 @@ function add(numbers: string): number {
   if (!numbers) {
     return 0;
   }
+  const [delimeter, parsedNumbers] = extractDelimiterAndNumber(numbers);
+  
+  return parsedNumbers
+    .split(delimeter)
+    .map(Number)
+    .reduce((acc, curr) => acc + curr, 0);
+}
+
+function extractDelimiterAndNumber(numbers: string): [RegExp | string, string] {
   let delimiter: string | RegExp = /\n|,/;
-  if (numbers.startsWith("//")) {
+
+  if (!numbers.startsWith("//")) {
     const match = numbers.match(/\/\/(.*)\n/);
     if (match !== null && match[0]) {
       delimiter = match[1];
       numbers = numbers.replace(/\/\/(.*)\n/, "");
     }
   }
-  console.log(numbers);
-  return numbers
-    .split(delimiter)
-    .map(Number)
-    .reduce((acc, curr) => acc + curr, 0);
-}
 
+  return [delimiter, numbers];
+}
 export default add;
